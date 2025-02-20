9c3e41b017577ab664cd5efaf420cb057a725fea
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-02-20 20:17:16 +0100

    Add the option to disable button once winner is announced
    Added a function to disable the buttons once a player has reached 5 points,so they cannot be clicked again

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const paper = document.querySelector("#paper");
 const rock = document.querySelector("#rock");
 const scissors = document.querySelector("#scissors");
 let displayResult = document.querySelector(".display-result");
-
 const paperPlayerSelection = getHumanChoice("paper");
 const rockPlayerSelection = getHumanChoice("rock");
 const scissorsPlayerSelection = getHumanChoice("scissors");
+const buttons = document.querySelectorAll("button");
+
 
 // Create the function that chooses randomly for the computer
 function getComputerChoice() {
@@ -20,20 +21,13 @@ function getHumanChoice(choice) {
     return choice;
 }
 
-// Initialize a boolean to be used in the while loop
-let stillRunning = true;
+ // Initilize the score variables and set their defualt value to 0
+ let humanScore = 0;
+ let computerScore = 0;
 
 // Create the function to play one round
 function playRound(humanChoice, computerChoice) {
     const choices = ["rock", "paper", "scissors"];
-    // Initilize the score variables and set their defualt value to 0
-    let humanScore = 0;
-    let computerScore = 0;
-
-    // if (!choices.includes(humanChoice) || humanChoice === ""){
-    //     const answer = console.log(`Please enter a valid choice!`);
-    //     return answer;
-    // };
 
     if(humanChoice === computerChoice){
         displayResult.innerText = `It's a tie!
@@ -43,6 +37,7 @@ function playRound(humanChoice, computerChoice) {
     }else if ((humanChoice === "rock" && computerChoice === "paper") 
             || (humanChoice === "paper" && computerChoice === "scissors") 
             || (humanChoice === "scissors" && computerChoice === "rock")) {
+
                 computerScore++;
                 displayResult.innerText = `You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}
                                             User's score is: ${humanScore}
@@ -55,14 +50,16 @@ function playRound(humanChoice, computerChoice) {
                                     Computer score is: ${computerScore}`;
     };
 
-    // if (humanScore === 5 || computerScore === 5){
-    //     console.log(`Thank you for playing! Final score is: 
-    //         User's score: ${humanScore} points
-    //         Computer's score: ${computerScore} points`)
-    //     stillRunning = false;
-    // }
-}
+    if (humanScore === 5 || computerScore === 5){
+        displayResult.innerText = `Thank you for playing! The winner after winning 5 rounds is: 
+            User's score: ${humanScore} points
+            Computer's score: ${computerScore} points`
 
+            buttons.forEach(button => button.disabled = true);
+    }
+
+    
+}
 
 
 // Create event listeners for the buttons
@@ -77,16 +74,3 @@ rock.addEventListener("click", ()=>{
 scissors.addEventListener("click", ()=>{
     playRound(scissorsPlayerSelection, getComputerChoice());
 });
-
-// // Create a function to play the game in a loop
-// function playGame() {
-
-    
-
-//     // while(stillRunning){
-//     //     playRound(getHumanChoice(), getComputerChoice());
-//     // }
-    
-// }
-
-// playGame();
