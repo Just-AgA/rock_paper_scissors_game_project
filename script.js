71f693ee223709719f818d5427a097c618eaa71b
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-02-20 19:23:11 +0100

    Change the playRound code to change the DOM
    Changed the playRound function so it changes the DOM instead of console.log the results

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const paper = document.querySelector("#paper");
+const rock = document.querySelector("#rock");
+const scissors = document.querySelector("#scissors");
+let displayResult = document.querySelector(".display-result");
+
+const paperPlayerSelection = getHumanChoice("paper");
+const rockPlayerSelection = getHumanChoice("rock");
+const scissorsPlayerSelection = getHumanChoice("scissors");
+
 // Create the function that chooses randomly for the computer
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -7,64 +16,77 @@ function getComputerChoice() {
 }
 
 // Create the function that gets the human choice
-function getHumanChoice() {
-    let choice = prompt("Please enter your choice:");
-    return choice.toLowerCase();
+function getHumanChoice(choice) {
+    return choice;
 }
 
-// Create a function to play the game in a loop
-function playGame() {
+// Initialize a boolean to be used in the while loop
+let stillRunning = true;
 
+// Create the function to play one round
+function playRound(humanChoice, computerChoice) {
+    const choices = ["rock", "paper", "scissors"];
     // Initilize the score variables and set their defualt value to 0
     let humanScore = 0;
     let computerScore = 0;
 
-    // Initialize a boolean to be used in the while loop
-    let stillRunning = true;
+    // if (!choices.includes(humanChoice) || humanChoice === ""){
+    //     const answer = console.log(`Please enter a valid choice!`);
+    //     return answer;
+    // };
 
-    // Create the function to play one round
-    function playRound(humanChoice, computerChoice) {
-        const choices = ["rock", "paper", "scissors"];
+    if(humanChoice === computerChoice){
+        displayResult.innerText = `It's a tie!
+                                    User's score is: ${humanScore}
+                                    Computer score is: ${computerScore}`;
 
-        if (!choices.includes(humanChoice) || humanChoice === ""){
-            const answer = console.log(`Please enter a valid choice!`);
-            return answer;
-        };
+    }else if ((humanChoice === "rock" && computerChoice === "paper") 
+            || (humanChoice === "paper" && computerChoice === "scissors") 
+            || (humanChoice === "scissors" && computerChoice === "rock")) {
+                computerScore++;
+                displayResult.innerText = `You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}
+                                            User's score is: ${humanScore}
+                                            Computer score is: ${computerScore}`;
+        
+    }else {
+        humanScore++;
+        displayResult.innerText = `You win! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}
+                                    User's score is: ${humanScore}
+                                    Computer score is: ${computerScore}`;
+    };
 
-        if(humanChoice === computerChoice){
-            console.log(`It's a tie!`);
-            console.log(`User's score is: ${humanScore}`);
-            console.log(`Computer score is: ${computerScore}`);
-            console.log("*********************");
-        }else if ((humanChoice === "rock" && computerChoice === "paper") 
-                || (humanChoice === "paper" && computerChoice === "scissors") 
-                || (humanChoice === "scissors" && computerChoice === "rock")) {
-                    computerScore++;
-                    console.log(`You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`);
-                    console.log(`User's score is: ${humanScore}`);
-                    console.log(`Computer score is: ${computerScore}`);
-                    console.log("*********************");
-            
-        }else {
-            humanScore++;
-            console.log(`You win! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`);
-            console.log(`User's score is: ${humanScore}`);
-            console.log(`Computer score is: ${computerScore}`);
-            console.log("*********************");
-        };
-
-        if (humanScore === 5 || computerScore === 5){
-            console.log(`Thank you for playing! Final score is: 
-                User's score: ${humanScore} points
-                Computer's score: ${computerScore} points`)
-            stillRunning = false;
-        }
-    }
-
-    while(stillRunning){
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    
+    // if (humanScore === 5 || computerScore === 5){
+    //     console.log(`Thank you for playing! Final score is: 
+    //         User's score: ${humanScore} points
+    //         Computer's score: ${computerScore} points`)
+    //     stillRunning = false;
+    // }
 }
 
-playGame();
+
+
+// Create event listeners for the buttons
+paper.addEventListener("click", ()=>{
+    playRound(paperPlayerSelection, getComputerChoice());
+});
+
+rock.addEventListener("click", ()=>{
+    playRound(rockPlayerSelection, getComputerChoice());
+});
+
+scissors.addEventListener("click", ()=>{
+    playRound(scissorsPlayerSelection, getComputerChoice());
+});
+
+// // Create a function to play the game in a loop
+// function playGame() {
+
+    
+
+//     // while(stillRunning){
+//     //     playRound(getHumanChoice(), getComputerChoice());
+//     // }
+    
+// }
+
+// playGame();
